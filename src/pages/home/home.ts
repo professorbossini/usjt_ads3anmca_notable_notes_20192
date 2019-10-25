a4ef6e3eee620e0b81af77be697ab1ecb905ff99
Rodrigo Bossini <professorbossini@gmail.com>
2019-10-24 21:18:21 -0300

    Paramos no passo 10

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  valor = 2;
+  notes = [
+    {
+      id: 1,
+      title: 'Firebase',
+      content: 'Estudar Firebase',
+      date: '2019-06-05' 
+    },
+    {
+    id: '2',
+    date: '2016-01-01',
+    title: 'Ionic',
+    content: 'Aprenda o básico de Ionic'
+    },
+    {
+    id: '3',
+    date: '2016-03-01',
+    title: 'Angular',
+    content: 'Importante para desenvolver com Ionic'
+    }
+  ]
 
+  //injeção de dependência
+  //é um tipo de inversão de controle
+  constructor( public navCtrl: NavController) {
+     
   }
 
+  
+
+  onItemClick (note){
+    console.log ("dentro do itemClick", note);  
+  }
 }
